5c6b58da10a73f2c33b03f2a941feff9d53f8935
OmarAlabdullah <omarkhaledal7@gmasil.com>
2023-06-10 14:59:42 +0200

    edit sprojectlist controller

--- admin.tester.test/controllers/project_lists_controller.php
+++ admin.tester.test/controllers/project_lists_controller.php
@@ -344,11 +344,16 @@
 					$additional_data_string = implode('|', array_filter(array_map('trim', $additional_data)));
 					echo $additional_data_string;
 					$additional_data_string = $db->real_escape_string($additional_data_string); // Escape the string to prevent SQL injection
+					try {
 
-					$query = ("UPDATE `project_lists` SET additional_data = '$additional_data_string' WHERE `project_id` = " . $project_list['Project_list']['id']);
-					$db->connect();
 
-					$db->query($query);
+						$query = ("UPDATE `project_lists` SET additional_data = '$additional_data_string' WHERE `project_id` = " . $project_list['Project_list']['id']);
+						$db->connect();
+
+						$db->query($query);
+					}catch (Error $e){
+						echo $e;
+				}
 
 					///////////////////////////////////////////////////////////////////////////////////
 
